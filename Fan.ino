9d6f4094e9202e4c515984c6f1a4dbe96aefd1a1
Ryan Bender <rbender4@liberty.edu>
2020-03-11 22:44:58 -0400

    added fan loop, fixed dependencies

--- Fan.ino
+++ Fan.ino
@@ -1,20 +1,9 @@
 /*
-  Fade
-
-  This example shows how to fade an LED on pin 9 using the analogWrite()
-  function.
-
-  The analogWrite() function uses PWM, so if you want to change the pin you're
-  using, be sure to use another PWM capable pin. On most Arduino, the PWM pins
-  are identified with a "~" sign, like ~3, ~5, ~6, ~9, ~10 and ~11.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fade
+    Loop made for kit fan
 */
 
-int led = 11;           // the PWM pin the LED is attached to
-int fan_speed = 0;    // how bright the LED is
+int led = 11;
+int fan_speed = 0;
 int curr_state = 0;
 
 // the setup routine runs once when you press reset:
